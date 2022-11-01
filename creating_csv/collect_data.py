8cd04b61feb4b39f11d41161ee997fb0b84b94ee
Butian <88835096+saliteta@users.noreply.github.com>
2022-11-01 11:35:05 +0800

    Add files via upload

---
+++ creating_csv/collect_data.py
@@ -0,0 +1,54 @@
+import numpy as np
+import cv2
+import mediapipe as mp
+from utils import save_csv
+
+
+mp_drawing= mp.solutions.drawing_utils
+# draw the detcted land mark directly on the screen
+mp_holistic = mp.solutions.holistic
+# detect the holitic model
+
+class_name = "communism"
+
+cap = cv2.VideoCapture(0)
+# Initiate holistic model
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    
+    count = 0
+    while cap.isOpened():
+        ret, frame = cap.read()
+        
+        # Recolor Feed
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False        
+        
+        # Make Detections
+        results = holistic.process(image)
+        # print(results.face_landmarks)
+        
+        # face_landmarks, pose_landmarks, left_hand_landmarks, right_hand_landmarks
+        
+        # Recolor image back to BGR for rendering
+        image.flags.writeable = True   
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        
+        # 1. Draw face landmarks
+        mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS, 
+                                 mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1),
+                                 mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
+                                 )
+        
+        
+        # Extract Face landmarks
+
+        
+        save_csv("happy", count, result=results)
+        count += 1
+        cv2.imshow('Raw Webcam Feed', image)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
